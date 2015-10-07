0304fd0ccf0202bcd7920c5aacd30fa2297b615f
dubem <dubem@dubemenyekwe.com>
2015-10-07 16:03:49 -0500

    Added model for Instruction

---
+++ Assembler/Model/Instruction.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SIC.Assembler.Model
+{
+    public class Instruction
+    {
+        private static Instructions _ALL = null;
+        private static Dictionary<string, Instruction> AllInstructions = new Dictionary<string, Instruction>();
+
+        private Instruction()
+        {
+        }
+
+        public static Instructions All
+        {
+            get
+            {
+                return _ALL ?? (_ALL = new Instructions());
+            }
+        }
+
+        public InstructionFormat Format { get; set; }
+
+        public string Mnemonic { get; set; }
+
+        public int OpCode { get; set; }
+
+        private static void RegisterInstruction(string mnemonic, int opcode, byte format)
+        {
+            AllInstructions.Add(mnemonic, new Instruction
+            {
+                Mnemonic = mnemonic,
+                OpCode = opcode,
+                Format = (InstructionFormat)Enum.Parse(typeof(InstructionFormat), format.ToString())
+            });
+        }
+
+        public sealed class Instructions
+        {
+            public Instruction this[string instructionStr]
+            {
+                get
+                {
+                    if (string.IsNullOrWhiteSpace(instructionStr))
+                    {
+                        return null;
+                    }
+
+                    var isFormat4 = instructionStr.StartsWith("+");
+                    var instruction = AllInstructions[instructionStr.TrimStart('+')];
+
+                    if (instruction == null)
+                    {
+                        return null;
+                    }
+
+                    // In other to avoid someone modifying the original values for the instruction
+                    // I'm returning a totally new object
+                    return new Instruction
+                    {
+                        Mnemonic = instruction.Mnemonic,
+                        OpCode = instruction.OpCode,
+                        Format = isFormat4 ? InstructionFormat.Four : instruction.Format
+                    };
+                }
+            }
+        }
+    }
+}
---
+++ Assembler/Model/InstructionFormat.cs
@@ -0,0 +1,33 @@
+﻿namespace SIC.Assembler.Model
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public enum InstructionFormat
+    {
+        /// <summary>
+        /// Unknown instuction format
+        /// </summary>
+        Unknown = 0,
+
+        /// <summary>
+        /// Instruction format 1
+        /// </summary>
+        One = 1,
+
+        /// <summary>
+        /// Instruction format 2
+        /// </summary>
+        Two = 2,
+
+        /// <summary>
+        /// Instruction format 3
+        /// </summary>
+        Three = 3,
+
+        /// <summary>
+        /// Instruction format 4
+        /// </summary>
+        Four = 4
+    }
+}
