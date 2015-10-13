a9d49d2059e1dfa6d9f9426ad184eb1d83e49fb1
dubem <dubem@dubemenyekwe.com>
2015-10-13 01:01:11 -0500

    Another Operand parse clean up.

--- Assembler/Providers/OperandEvaluator.cs
+++ Assembler/Providers/OperandEvaluator.cs
@@ -7,13 +7,6 @@ namespace SIC.Assembler.Providers
 {
     public class OperandEvaluator
     {
-        private static string EXPRESSION_OPERATORS = "+-";
-        private static string IMMEDIATE_VALUE_TOKEN = "#";
-        private static string INDEXED_ADDRESSING_TOKEN = ",";
-        private static string INDIRECT_ADDRESSING_TOKEN = "@";
-        private static string LITERAL_NUMBER_TOKEN = "=x";
-        private static string LITERAL_STRING_TOKEN = "=c";
-
         public static LiteralTable ParseExpressions(IList<string> expressions, SymbolTable symbolTable, Action<object> printFunction, Action<object> errorFunction)
         {
             if (expressions == null || symbolTable == null)
